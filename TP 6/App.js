341d3f82801d1728c1832444d2981463a721c91e
Delfina Munich <89984382+delfiam@users.noreply.github.com>
2022-10-06 09:32:11 -0300

    ayuda

--- TP 6/App.js
+++ TP 6/App.js
@@ -1,12 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import {useState, useEffect} from 'react';
+import RNFS from 'react-native-fs';
 
 export default function App() {
+  const [downloadsFolder, setDownloadsFolder] = useState('');
+  const [documentsFolder, setDocumentsFolder] = useState('');
+  const [externalDirectory, setExternalDirectory] = useState('');
+  useEffect(() => {
+    //get user's file paths from react-native-fs
+    setDownloadsFolder(RNFS.DownloadDirectoryPath);
+    setDocumentsFolder(RNFS.DocumentDirectoryPath); //alternative to MainBundleDirectory.
+    setExternalDirectory(RNFS.ExternalStorageDirectoryPath);
+  }, []);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <Text> Downloads Folder: {downloadsFolder}</Text>
+      <Text>Documents folder: {documentsFolder}</Text>
+      <Text>External storage: {externalDirectory}</Text>
+    </SafeAreaView>
   );
 }
 
