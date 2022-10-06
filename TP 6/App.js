1dc0b375f526e9b531d5affe6074eed1c25cc34f
Delfina Munich <89984382+delfiam@users.noreply.github.com>
2022-10-06 10:34:55 -0300

    nose

--- TP 6/App.js
+++ TP 6/App.js
@@ -1,23 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
+import { Dirs, FileSystem } from 'react-native-file-access';
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import {useState, useEffect} from 'react';
 import RNFS from 'react-native-fs';
 
 export default function App() {
-  const [downloadsFolder, setDownloadsFolder] = useState('');
-  const [documentsFolder, setDocumentsFolder] = useState('');
-  const [externalDirectory, setExternalDirectory] = useState('');
-  useEffect(() => {
-    //get user's file paths from react-native-fs
-    setDownloadsFolder(RNFS.DownloadDirectoryPath);
-    setDocumentsFolder(RNFS.DocumentDirectoryPath); //alternative to MainBundleDirectory.
-    setExternalDirectory(RNFS.ExternalStorageDirectoryPath);
-  }, []);
+  const text = FileSystem.readFile(Dirs.CacheDir + '/test.txt');
   return (
     <SafeAreaView>
-      <Text> Downloads Folder: {downloadsFolder}</Text>
-      <Text>Documents folder: {documentsFolder}</Text>
-      <Text>External storage: {externalDirectory}</Text>
+      <Text> {text}</Text>
     </SafeAreaView>
   );
 }
