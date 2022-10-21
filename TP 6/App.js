d53dc3cf27f61080a8464c001c1c993200c4ee86
Delfi <delfinamunich@gmail.com>
2022-10-21 16:54:36 -0300

    el image picker está al menos TT

--- TP 6/App.js
+++ TP 6/App.js
@@ -1,23 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { Dirs, FileSystem } from 'react-native-file-access';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
-import {useState, useEffect} from 'react';
-import RNFS from 'react-native-fs';
+import React, { useState, useEffect } from 'react';
+import { Button, Image, View, Platform } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 
-export default function App() {
-  const text = FileSystem.readFile(Dirs.CacheDir + '/test.txt');
+export default function ImagePickerExample() {
+  const [image, setImage] = useState(null);
+
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+  };
+
   return (
-    <SafeAreaView>
-      <Text> {text}</Text>
-    </SafeAreaView>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button title="Pick an image from camera roll" onPress={pickImage} />
+      {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
