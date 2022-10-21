aa84bca1cde4216d3a54cccad57f090447e305a5
Delfi <delfinamunich@gmail.com>
2022-10-21 18:15:47 -0300

    TERMINADO TP 6

--- TP 6/App.js
+++ TP 6/App.js
@@ -1,30 +1,38 @@
-import React, { useState, useEffect } from 'react';
-import { Button, Image, View, Platform } from 'react-native';
-import * as ImagePicker from 'expo-image-picker';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Button, View } from 'react-native';
+import * as FileSystem from 'expo-file-system';
+import * as Sharing from 'expo-sharing';
+import pptxgen from 'pptxgenjs';
 
-export default function ImagePickerExample() {
-  const [image, setImage] = useState(null);
-
-  const pickImage = async () => {
-    // No permissions request is necessary for launching the image library
-    let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.All,
-      allowsEditing: true,
-      aspect: [4, 3],
-      quality: 1,
+export default function App() {
+  const GenerarPower = () => { // <-- Crea un ppt
+    let ppt = new pptxgen();
+    let slide = ppt.addSlide(); // <-- Agrega una diapositiva
+    slide.background = { path: "https://images4.alphacoders.com/980/thumb-1920-980848.jpg" };
+    slide.addText("Bienvenidos a mi PowerPoint:)", {x: 0.5, y: 1.0, fontSize: 50, color: 'ffffff'});
+    ppt.write("base64").then(base64 => {
+      const filename = FileSystem.documentDirectory + "TP6.pptx"; // <-- Crea un archivo pptx
+      FileSystem.writeAsStringAsync(filename, base64, {
+        encoding: FileSystem.EncodingType.Base64 // <-- Escribe el archivo con FS
+      }).then(() => {
+        Sharing.shareAsync(filename); // <-- Comparte el archivo
+      });
     });
-
-    console.log(result);
-
-    if (!result.cancelled) {
-      setImage(result.uri);
-    }
   };
 
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button title="Pick an image from camera roll" onPress={pickImage} />
-      {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+    <View style={styles.container}>
+      <Button title="Generar PowerPoint" onPress={GenerarPower} />
+      <StatusBar style="auto" />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
